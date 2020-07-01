32d46b62ac92dc044bf951710b5d87bd7c816cf8
MOHAMMED <mohammed@MOHAMMEDs-MacBook-Pro.local>
2020-07-01 21:27:50 +0200

    eerste commit

---
+++ src/Component/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1> About</h1>
+            <p>this is About section</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/Component/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link,NavLink} from 'react-router-dom'
+//NavLink same like Link but  add class 
+//  if we want to change the className frome active to Mohammed we can use >>  activeClassName="mohammed"
+function Nav() {
+    return (
+        <div className='navbar'>
+            <div className='container'>
+                <a href='#' className='logo'> logo</a>
+                <ul>
+                    
+                    <NavLink exact to='/'> Home</NavLink> 
+                    <NavLink to='/About'>About</NavLink>
+                </ul>
+            </div>
+           
+        </div>
+    )
+}
+
+export default Nav
